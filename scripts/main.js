e1c4b563347c1a39725490c8de56882a852385cd
Kryotasin <kshitij.sinha94@gmail.com>
2019-09-26 02:08:36 -0700

    Users variable in js file now working

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-
 function showHide(inputId) {
   var x = document.getElementById(inputId);
   if (x.style.display === "none") {
@@ -8,7 +7,7 @@ function showHide(inputId) {
   }
 }
 
-function createQuestionForm(){
+function createQuestionForm() {
   // Number of inputs to create
   var users = document.getElementById('userCount').value;
   var questions = document.getElementById('questionCount').value;
@@ -20,44 +19,44 @@ function createQuestionForm(){
   var form = document.getElementById('container');
 
   while (form.hasChildNodes()) {
-      form.removeChild(form.lastChild);
+    form.removeChild(form.lastChild);
   }
 
-  for(j=0;j<2;j++){
+  for (j = 0; j < users; j++) {
     var form = document.createElement("form");
     form.className = "form-horizontal";
     container.appendChild(form);
 
     var legend = document.createElement("legend");
-    legend.innerHTML = "Enter Data for User " + (j+1);
+    legend.innerHTML = "Enter Data for User " + (j + 1);
     form.appendChild(legend);
 
 
-        for (i=0;i<questions;i++){
-            //form-group
-            var class1 = document.createElement("div");
-            class1.className = "form-group";
-            form.appendChild(class1);
+    for (i = 0; i < questions; i++) {
+      //form-group
+      var class1 = document.createElement("div");
+      class1.className = "form-group";
+      form.appendChild(class1);
 
-            //label
-            var label = document.createElement("label");
-            label.className = "col-md-4 control-label";
-            label.innerHTML = "Question Number" + (i+1) + ": ";
-            label.htmlFor = "member" + i;
-            class1.appendChild(label);
+      //label
+      var label = document.createElement("label");
+      label.className = "col-md-4 control-label";
+      label.innerHTML = "Question Number" + (i + 1) + ": ";
+      label.htmlFor = "member" + i;
+      class1.appendChild(label);
 
-            //second div
-            var class2 = document.createElement("div");
-            class2.className = "col-md-2";
-            class1.appendChild(class2);
+      //second div
+      var class2 = document.createElement("div");
+      class2.className = "col-md-2";
+      class1.appendChild(class2);
 
 
-            // Create an <input> element, set its type and name attributes
-            var input = document.createElement("input");
-            input.type = "number";
-            input.name = "member" + i;
-            input.className = "form-control input-md";
-            class2.appendChild(input);
-        }
-      }
+      // Create an <input> element, set its type and name attributes
+      var input = document.createElement("input");
+      input.type = "number";
+      input.name = "member" + i;
+      input.className = "form-control input-md";
+      class2.appendChild(input);
+    }
+  }
 }
